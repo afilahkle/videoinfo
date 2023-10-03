3eb664d7754d0c90695610cfc8cb880d84e02465
Omar Khalifa <elkhalifa002@gmail.com>
2023-10-03 19:30:36 +0300

    Add preload script to Electron app and load index.html from app directory

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const electron = require('electron');
+const path = require('path');
 
 const { app, BrowserWindow } = electron;
 
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({});
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: path.join(app.getAppPath(), 'preload.js'),
+    },
+  });
+
+  mainWindow.loadURL(path.join(app.getAppPath(), 'index.html'));
 });
