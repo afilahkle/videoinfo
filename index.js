5bc07889d76f5bdfbcb1c3b1c28e1c3c2077d346
Omar Khalifa <elkhalifa002@gmail.com>
2023-10-03 22:07:06 +0300

    Add context isolation and remote module disabling to improve security in Electron app

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const electron = require('electron');
 const path = require('path');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({
     webPreferences: {
       preload: path.join(app.getAppPath(), 'preload.js'),
+      contextIsolation: true,
+      enableRemoteModule: false,
     },
   });
 
   mainWindow.loadURL(path.join(app.getAppPath(), 'index.html'));
+
+  ipcMain.on('video:submit', (event, path) => {
+    console.log(p);
+  });
 });
