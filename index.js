b1ea28680509c0a69edba5ee9289a04d4fb25084
Omar Khalifa <elkhalifa002@gmail.com>
2023-10-04 09:24:37 +0300

    Add support for video metadata retrieval using fluent-ffmpeg

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const electron = require('electron');
+const { ffprobe } = require('fluent-ffmpeg/lib/fluent-ffmpeg');
 const path = require('path');
 
 const { app, BrowserWindow, ipcMain } = electron;
 
+let mainWindow;
+
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     webPreferences: {
       preload: path.join(app.getAppPath(), 'preload.js'),
       contextIsolation: true,
       enableRemoteModule: false,
     },
   });
-
   mainWindow.loadURL(path.join(app.getAppPath(), 'index.html'));
 
   ipcMain.on('video:submit', (event, path) => {
-    console.log(path);
+    ffprobe(path, (err, metadata) => {
+      mainWindow.webContents.send('video:info', {
+        duration: metadata.format.duration,
+      });
+    });
   });
 });
